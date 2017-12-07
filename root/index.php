709718549f98419bac2cacfb4b0fecb7528b4a71
Samme Robbroeckx <Samme.Robbroeckx@securitas.be>
2017-12-07 09:15:50 +0100

    added active collapse

--- root/index.php
+++ root/index.php
@@ -21,6 +21,30 @@
 	
 	<!-- AJAX -->
 	<script>
+		function showActive() 
+		{
+			if (window.XMLHttpRequest) 
+			{
+				// code for IE7+, Firefox, Chrome, Opera, Safari
+				xmlhttp = new XMLHttpRequest();
+			} 
+			else 
+			{
+				// code for IE6, IE5
+				xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+			}
+			xmlhttp.onreadystatechange = function() 
+			{
+				if (this.readyState == 4 && this.status == 200) 
+				{
+					document.getElementById("ShowActive").innerHTML = this.responseText;
+				}
+			};
+			
+			xmlhttp.open("GET","PHP/ShowActive.php",true);
+			xmlhttp.send();
+		}
+	
 		function showSearch(str) 
 		{
 			if (str == "") 
@@ -54,7 +78,7 @@
 		}
 	</script>
   </head>
-  <body>
+  <body onload="showActive()">
     <h1 class="text-center">Activiteiten</h1>
 	
 	<div class="container">
@@ -70,6 +94,10 @@
 						</span>
 					</div>
 					<br>
+					
+					<div id="ShowActive">
+					
+					</div>
 					
 					<div id="ShowSearchList">
 					
